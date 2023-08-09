e530197b6057605dc1731f957df1ffca906da253
jefflangtech <jeff.m.lang@gmail.com>
2023-08-08 20:49:02 -0700

    Explored the pub-sub pattern for menu functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,52 @@
-// const menu = document.getElementById('drawer-menu');
-// const toggle = document.getElementById(menu.dataset.menuToggle);
+const PubSub = {
 
-const slidingDrawer = function(
-  contentEl, 
-  triggerEl, 
-  slideFunction, 
-  modifyTriggerFunction) {
+  events: {},
 
-    this.drawer = contentEl;
-    this.trigger = triggerEl;
-    this.drawerSlide = slideFunction;
-    this.toggleTrigger = modifyTriggerFunction;
+  subscribe: function(eventName, fn) {
+    this.events[eventName] = this.events[eventName] || [];
+    this.events[eventName].push(fn);
+  },
+
+  unsubscribe: function(eventName, fn) {
+    if(this.events[eventName]) {
+      this.events[eventName] = this.events[eventName].filter(
+        subscriber => subscriber !== fn);
+    }
+  },
+
+  publish: function(eventName, data) {
+    if(this.events[eventName]) {
+      this.events[eventName].forEach(subscriber => {
+        subscriber(data)
+      });
+    }
+  }
 
 };
+
+const SlidingDrawer = function(contentEl, slideFunction) {
+  this.drawer = contentEl;
+  this.drawerSlide = slideFunction;
+};
+
+const DrawerTrigger = function(triggerEl, modTriggerFn) {
+  this.trigger = triggerEl;
+  this.modTrigger = modTriggerFn;
+};
+
+const testMenu = new SlidingDrawer(
+  document.getElementById('drawer-menu'),
+  drawerSlide
+);
+
+const testTrigger = new DrawerTrigger(
+  document.getElementById('drawer-menu-toggle'), 
+  modDrawerTrigger
+);
+
+// Subscribing the drawerSlide method to the event
+// It has to have a bound this because of how it gets called
+PubSub.subscribe('testTriggerClicked', drawerSlide.bind(testMenu));
 
 function drawerSlide() {
 
@@ -26,17 +60,35 @@ function modDrawerTrigger() {
 
 }
 
-const testMenu = new slidingDrawer(
-  document.getElementById('drawer-menu'), 
-  document.getElementById('drawer-menu-toggle'), 
-  drawerSlide, 
-  modDrawerTrigger);
-
-console.dir(testMenu);
-
-testMenu.trigger.addEventListener('click', (event) => {
-
-  testMenu.toggleTrigger();
-  testMenu.drawerSlide();
-
+testTrigger.trigger.addEventListener('click', () => {
+  PubSub.publish('testTriggerClicked');
 });
+
+
+// const slidingDrawer = function(
+//   contentEl, 
+//   triggerEl, 
+//   slideFunction, 
+//   modifyTriggerFunction) {
+
+//     this.drawer = contentEl;
+//     this.trigger = triggerEl;
+//     this.drawerSlide = slideFunction;
+//     this.toggleTrigger = modifyTriggerFunction;
+
+// };
+
+// const testMenu = new slidingDrawer(
+//   document.getElementById('drawer-menu'), 
+//   document.getElementById('drawer-menu-toggle'), 
+//   drawerSlide, 
+//   modDrawerTrigger);
+
+// console.dir(testMenu);
+
+// testMenu.trigger.addEventListener('click', (event) => {
+
+//   testMenu.toggleTrigger();
+//   testMenu.drawerSlide();
+
+// });
